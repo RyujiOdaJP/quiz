36f2ca4ea0fd9b7ea76591e2e12a2b5ce231ad2b
ryujioda <ryuji.oda@gmail.com>
2021-02-15 03:36:51 +0900

    set values

--- app/src/main/java/com/example/quiz/QuizQustionActivity.kt
+++ app/src/main/java/com/example/quiz/QuizQustionActivity.kt
@@ -3,6 +3,7 @@ package com.example.quiz
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import kotlinx.android.synthetic.main.activity_quiz_qustion.*
 
 class QuizQustionActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,5 +12,19 @@ class QuizQustionActivity : AppCompatActivity() {
 
         val questionList = Constants.getQuestions()
         Log.d("Question Size", "${questionList.size}")
+
+        val currentPosition = 1
+        val question: Question = questionList[currentPosition - 1]
+
+        progressBar.progress = currentPosition
+        tvProgress.text = "${currentPosition}" + "/" + "${progressBar.max}"
+
+        tvQuestion.text = question.question
+        ivImage.setImageResource(question.image)
+        tv_option_one.text = question.optionOne
+        tv_option_two.text = question.optionTwo
+        tv_option_three.text = question.optionThree
+        tv_option_four.text = question.optionFour
+        btnSubmit.text = "SUBMIT"
     }
 }
